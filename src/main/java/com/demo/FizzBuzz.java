aa7f8020728a830cadce99a29eaaecdb460573fe
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2022-03-30 14:47:56 -0400

    fixed variable name in processNumbers

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -18,12 +18,12 @@ public class FizzBuzz
 	String processNumbers (int[] numList)
 	{
     	// Copy your code here
-		int length = nums.length;
+		int length = numList.length;
         String result = "";
         for(int i = 0; i < length-1; i++){
-          result += (String) processNumber(nums[i]) + ", ";
+          result += (String) processNumber(numList[i]) + ", ";
         }
-        return result + (String) processNumber(nums[length-1]);
+        return result + (String) processNumber(numList[length-1]);
 	}
 
 }
