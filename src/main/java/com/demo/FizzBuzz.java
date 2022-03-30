b800efbdc284a35669463f6e5740df57716d7b4f
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2022-03-30 14:46:01 -0400

    copied code into fizzbuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -5,13 +5,25 @@ public class FizzBuzz
     String processNumber(int num)
 	{	
     	// Copy your code here
-		return "";
+		if (num%3 == 0 && num%5 == 0){
+          return "Fizz Buzz";
+        } else if (num%3 == 0) {
+          return "Fizz";
+        } else if (num%5 == 0) {
+          return "Buzz";
+        } else
+          return ""+num;
 	}
 	
 	String processNumbers (int[] numList)
 	{
     	// Copy your code here
-		return "";
+		int length = nums.length;
+        String result = "";
+        for(int i = 0; i < length-1; i++){
+          result += (String) processNumber(nums[i]) + ", ";
+        }
+        return result + (String) processNumber(nums[length-1]);
 	}
 
 }
